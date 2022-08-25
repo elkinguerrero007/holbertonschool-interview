7fd4d510c34c12150fa2019b810bb0e58310b0d8
elkinguerrero007 <3439@holbertonschool.com>
2022-08-24 22:37:04 -0500

    machine_learning

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,1 +1,27 @@
-...
+#!/usr/bin/python3
+"""
+Calculates the minimum operations
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations needed to result in exactly n
+    H characters in the file
+    :param n: the number of characters to be printed
+    :return: the minimum number of operations
+    """
+    if type(n) is not int or n < 2:
+        return 0
+    number = n
+    result = 0
+    while number % 2 == 0:
+        number = number / 2
+        result += 2
+    for i in range(3, int(number ** (1 / 2)) + 1, 2):
+        while number % i == 0:
+            result = result + i
+            number = number / i
+    if number > 2:
+        result += number
+    return int(result)
