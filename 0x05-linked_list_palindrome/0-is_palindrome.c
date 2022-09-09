c7abd6291572777a42dddf697c52eee85a51ffd4
elkinguerrero007 <3439@holbertonschool.com>
2022-09-08 22:06:44 -0500

    palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - function main 
+ * @head: Pointer to the head of the list
+ * Return: 1 or 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int array[2048], i = 0, j = 0, middle;
+
+	if (head == NULL || (tmp != NULL && tmp->next == NULL))
+		return (1);
+	while (tmp != NULL)
+	{
+		array[i] = tmp->n;
+		tmp = tmp->next;
+		i++;
+	}
+	i--, middle = i / 2;
+	while (i >= middle && j <= middle)
+	{
+		if (array[j] != array[i])
+			return (0);
+		i--, j++;
+	}
+	return (1);
+}
