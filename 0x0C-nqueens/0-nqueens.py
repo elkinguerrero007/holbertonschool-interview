79da74b718d87b55833810cdd2f30c2092a4cbd8
elkinguerrero007 <3439@holbertonschool.com>
2022-11-03 14:45:07 -0500

    uppdate

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,97 +1,43 @@
 #!/usr/bin/python3
-"""
-Solving N Queens problem using backtracking algorithm
-- 0x0C-nqueens module
-- Holberton School
-- Python Core - 0x0C
-- N Queens
-- Author: JuanDAC
-- Date: July 2022
-- File: 0x0C-nqueens.py
-"""
+'''
+Module that solves the N Queens puzzle
+'''
 from sys import argv, exit
 
-
-def is_safe(y, x, board):
-    """
-    Method:
-        is_safe(n: int, y: int, x: int, board: list) -> bool
-    Description:
-        This method checks if a queen can be placed on
-        a given board at a given position. It checks if
-        the queen is attacking any other piece on the board.
-    Args:
-        y {int}
-            current row
-        x {int}
-            current column
-        board {List[int]}
-            current board
-    Print:
-        - True if the queen can be placed on the board, False otherwise
-    Return:
-        - bool
-    """
-
-    for cell in board:
-        if x == cell[1]:
-            return False
-        if y - x == cell[0] - cell[1]:
-            return False
-        if x - cell[1] == cell[0] - y:
-            return False
-    return True
-
-
-def nqueens(n, y, board):
-    """
-    Method:
-        nqueens(n: int, y: int, board: list) -> void
-    Description:
-        This method is a recursive method that
-        places n queens on an n by n board so
-        that no queens are attacking any other
-        piece on the board. It uses backtracking
-        to find a solution.
-    Args:
-        n {int}
-            number of queens
-        y {int}
-            current row
-        board {List[int]}
-            current board
-    Print:
-        - All possible solutions for n queens on an n by n board
-    Return:
-        - void
-    """
-
-    for x in range(n):
-        if is_safe(y, x, board):
-            board.append([y, x])
-            if y == n - 1:
-                print(board)
-                del board[-1]
-                break
-            nqueens(n, y + 1, board)
-            del board[-1]
-
-
-if __name__ == '__main__':
-    """
-    Main guard - check for correct number of arguments and call nqueens
-    """
-    n = 0
+if __name__ == "__main__":
     if len(argv) != 2:
-        print("Usage: nqueens N")
+        print('Usage: nqueens N')
         exit(1)
     try:
         n = int(argv[1])
-    except Exception:
+    except BaseException:
         print('N must be a number')
         exit(1)
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         exit(1)
 
-    nqueens(n, 0, [])
+    solution = []
+
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
+
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
+
+    solve_queens(0, n, solution)
