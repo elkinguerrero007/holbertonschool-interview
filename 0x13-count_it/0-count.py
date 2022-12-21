104bd470eb4b9f5b34f655cb44af689d9c7e9ecb
elkinguerrero007 <3439@holbertonstudents.com>
2022-12-21 12:03:20 -0500

    interview

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,53 +1,42 @@
 #!/usr/bin/python3
-""" Count it module """
+"""This module contains the count_words function"""
+
 import requests
 
 
-def count_words(subreddit, word_list, after="", word_counter=None):
-    """
-    Function sorted by number of occurences and
-    alphabetically for each article
-    Arguments:
-        - subreddit: subreddit to search
-        - word_list: list of words to search for
-        - after: after parameter for pagination
-        - word_counter: dictionary of words and their count
-    Returns:
-        - None if subreddit is invalid or no posts match
-    """
-    if after is None:
-        return None
-    if word_counter is None:
-        word_counter = {}
-        for word in word_list:
-            word_counter[word] = 0
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) \
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
-    }
-    params = {
-        "limit": 100,
-        "after": after
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+def get_articles(sub, w, wd, wd_r, a="",):
+    """ Get articles """
+    url = "https://www.reddit.com/r/{}".format(sub)
+    url = "{}/hot.json?limit=100&after={}".format(url, a)
+    response = requests.get(url, allow_redirects=False,
+                            headers={'User-agent': 'Holberton'})
     if response.status_code != 200:
         return None
-    response = response.json()
-    data = response.get("data")
-    after = data.get("after")
-    posts = data.get("children")
-    for post in posts:
-        post = post.get("data")
-        title = post.get("title").lower()
-        title = title.replace(".", " ").replace("!", " ").replace("_", " ")
-        title = title.split(" ")
-        for word in title:
-            if word in word_counter:
-                word_counter[word] += 1
-    count_words(subreddit, word_list, after, word_counter)
-    if after is None:
-        for word in sorted(word_counter, key=word_counter.get, reverse=True):
-            if word_counter[word] != 0:
-                print("{}: {}".format(word, word_counter[word]))
+    articles = response.json()["data"]["children"]
+    for article in articles:
+        titles = article["data"]["title"].lower().split(" ")
+        for title in titles:
+            if title in wd:
+                wd[title] += 1
+    if response.json()["data"]["after"] is None:
+        sw = sorted(wd.items(), key=lambda t: t[::-1])
+        sw_desc = sorted(sw, key=lambda tup: tup[1], reverse=True)
+        for w in sw_desc:
+            if w[1] > 0:
+                print("{}: {}".format(w[0], w[1] * wd_r[w[0]]))
+        return
+    return get_articles(sub, w, wd, wd_r, response.json()["data"]["after"])
+
+
+def count_words(subreddit, word_list):
+    """ Count words """
+    w_d = {}
+    w_d_r = {}
+    word_list = [word.lower() for word in word_list]
+    for w in word_list:
+        if w not in w_d:
+            w_d_r[w] = 1
+            w_d[w] = 0
+        else:
+            w_d_r[w] += 1
+    get_articles(subreddit, word_list, w_d, w_d_r)
