a9c596161e1340646f4fa603cb44a4b296d55f33
elkinguerrero007 <3439@holbertonstudents.com>
2023-02-23 12:54:17 -0500

    making_change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
-"""Module used to add two arrays."""
+"""
+Main file for testing
+"""
 
 
 def makeChange(coins, total):
-    """[Given a pile of coins of different values, determine the fewest number
-            of coins needed to meet a given amount total]
-    Args:
-            coins ([list]): [list of the values of your the coins]
-                              The value of a coin will always be an int > 0
-            total ([type]): [description]
-    Returns:
-            c [int]: (change  [fewest number of coins needed to meet total]
     """
-
+    change
+    """
     if total <= 0:
         return 0
-
-    # verify coins is a valid
-    if (coins is None or len(coins) == 0):
+    matriz = [float('inf')] * (total + 1)
+    matriz[0] = 0
+    for i in range(len(coins)):
+        for j in range(coins[i], total + 1):
+            matriz[j] = min(matriz[j], matriz[j - coins[i]] + 1)
+    if matriz[total] != float('inf'):
+        return matriz[total]
+    else:
         return -1
-
-    change = 0
-    my_coins = sorted(coins, reverse=True)
-    money_left = total
-
-    for coin in my_coins:
-        while (money_left % coin >= 0 and money_left >= coin):
-            change += int(money_left / coin)
-            money_left = money_left % coin
-
-    change = change if money_left == 0 else -1
-
-    return change
