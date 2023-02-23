179281371657450740ef481f2c45430828b2e8a1
elkinguerrero007 <3439@holbertonstudents.com>
2023-02-23 13:12:01 -0500

    making_change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
 """
-making change module
+Change comes from within
+Time Complexity O(n * log(n))
+Space complexity O(1)
 """
 
 
-def makeChange(coins, total):
+def makeChange(coins: list, total: int) -> int:
     """
-    make change: bottom up method
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Args:
+        coins (list): Is a list of the values of the coins in
+        your possession
+        total (int):  Total number to reach
+    Returns:
+        int: Fewest number of coins needed to meet total
     """
     if total <= 0:
         return 0
-    if total in coins:
-        return 1
-    if len(coins) <= 0:
-        return -1
-    dp = [0] * (total + 1)
-    for i in range(1, total + 1):
-        dp[i] = total + 1
-    dp[0] = 0
-    for a in range(1, total + 1):
-        for coin in coins:
-            if a - coin >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - coin])
-    return dp[total] if dp[total] != total + 1 else -1
+    coins.sort(reverse=True)
+    number = 0
+    for i in coins:
+        while i <= total:
+            total -= i
+            number += 1
+        if total == 0:
+            return number
+    return -1
+
+
+if __name__ == "__main__":
+    coins = [1, 2, 5, 10, 25]
+    total = 47
+    print(makeChange(coins, total))
